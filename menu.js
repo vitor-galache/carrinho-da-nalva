f1fc23d148bbbe92d271ef2d1633750812e1449d
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-19 21:56:17 -0300

    Inicializando repositório do projeto

---
+++ menu.js
@@ -0,0 +1,3 @@
+document.getElementById('voltar').addEventListener('click', function () {
+    window.location.href = 'index.html'; // Substitua pela URL da página inicial
+});
---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('verCardapio').addEventListener('click', function () {
+    const homeContent = document.querySelector('.home');
+
+    // Adiciona a classe slide-up para mover a página para cima
+    homeContent.classList.add('slide-up');
+
+    // Redireciona após o término da animação (500ms)
+    setTimeout(function () {
+        window.location.href = 'menu.html'; // Substituir pela URL do cardápio
+    }, 410); // Tempo igual ao da animação no CSS
+});
