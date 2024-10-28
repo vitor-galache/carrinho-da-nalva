268a7712da16e92a8b6579a852a4e5f69721d8ca
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-28 12:36:16 -0300

    Feature: Menu interativo

--- scripts/menu-bebidas.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btn-proxima').addEventListener('click', function () {
-    window.location.href = 'menu-porcoes.html'; // Substitua pela URL da página inicial
-});
-
--- scripts/menu-porcoes.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('btn-voltar').addEventListener('click', function () {
-    window.location.href = 'index.html'; // Substitua pela URL da página inicial
-});
